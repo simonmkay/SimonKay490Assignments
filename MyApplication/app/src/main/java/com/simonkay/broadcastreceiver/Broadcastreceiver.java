215f2e070666d4289b8ab89021aa78f17862b07f
simonmkay <simonmkay@yahoo.com>
2014-11-24 18:18:22 -0500

    Totally the last one
    Probably not the last one...

--- MyApplication/app/src/main/java/com/simonkay/broadcastreceiver/Broadcastreceiver.java
+++ MyApplication/app/src/main/java/com/simonkay/broadcastreceiver/Broadcastreceiver.java
@@ -9,10 +9,10 @@ import android.widget.Toast;
 /**
  * Created by Simon Kay on 11/24/2014.
  */
-public class Broadcastreceiver {
+public class Broadcastreceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(Context, "We sense an Intent.", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "We sense an Intent.", Toast.LENGTH_LONG).show();
     }
 
 }
